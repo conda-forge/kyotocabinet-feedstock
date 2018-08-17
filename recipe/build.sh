7ec1395e58b9340ed8e433e5cf3e76b6da611589
Yuriy Davygora <yuriy.davygora@trustyou.net>
2018-08-17 09:55:16 +0200

    Fixing the arch problem, building it with -march=nocona instead of -march=native now

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,8 +2,12 @@
 
 set -e
 
+export CFLAGS=-march=nocona
+export CXXFLAGS=-march=nocona
+
 ./configure --prefix=$PREFIX
 
 make
 make check &> make_check.log || { cat make_check.log; exit 1; }
 make install
+
